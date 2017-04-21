216c5e74756a4ebf623df680bb2771338cec8ef9
Roberto Ariosa <rob.ariosa@hotmail.com>
2017-04-21 13:24:49 +0200

    Especie - private

--- Especie.hh
+++ Especie.hh
@@ -11,3 +11,26 @@
 #include <stdio.h>
 
 #endif /* Especie_hh */
+
+class Especie {
+  
+  // Tipo de modulo: datos
+  /*
+   Descripcion del tipo: almacena los datos genericos
+   de una especie concreta.
+   */
+  
+private:
+  int N, li, lx, ly;
+  
+  /*
+   
+   Invariante de la representacion:
+   - 0 <= N
+   - li (0 <= i <= N)
+   
+   */
+  
+public:
+  
+};
